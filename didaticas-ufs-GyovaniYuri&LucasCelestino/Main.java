3d2da5f02623450132624702d26df909f029ab9e
Gyovani Y. S. Santos <39191743+gyovani19@users.noreply.github.com>
2023-08-31 16:30:55 -0300

    Matriculas e Nomes da Dupla

--- didaticas-ufs-GyovaniYuri&LucasCelestino/Main.java
+++ didaticas-ufs-GyovaniYuri&LucasCelestino/Main.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 import java.util.HashMap;
-
+//Alunos:
+//Gyovani Yuri Souza Santos - 202200116909
+//Lucas Conceição Celestino - 202200060071
 public class Main {
     public static void main(String[] args) {
         Game game = new Game();
